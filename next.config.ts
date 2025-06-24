0df6d79a9457eeb6e0cee3a609d72e70d10279be
abudi <abudi4947@gmail.com>
2025-06-24 20:52:13 +0300

    removed eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Disable ESLint during builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Optionally disable TypeScript type checking during builds too
+    ignoreBuildErrors: false, // Set to true if you want to disable TypeScript errors as well
+  },
 };
 
 export default nextConfig;
